a96fd47a869cadb3456cc0c7137b56efa459b417
Sakura <Sakura@163.com>
2020-01-31 18:19:32 +0800

    fix：修复无关紧要的语法报错

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,22 +1,22 @@
 <template>
   <div>
     Home
-    <el-button type="info" @click="logout">退出</el-button>
+    <el-button @click="logout" type="info">退出</el-button>
   </div>
 </template>
 
 <script>
-export default {
-  methods: {
-    logout () {
-      window.sessionStorage.clear()
-      this.$message.success('退出成功！')
-      this.$router.push('/login')
+  export default {
+    methods: {
+      logout () {
+        window.sessionStorage.clear()
+        this.$message.success('退出成功！')
+        this.$router.push('/login')
+      }
     }
   }
-}
 </script>
 
-<style scoped lang="less">
+<style lang="less" scoped>
 
 </style>
