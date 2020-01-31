c655f2b8ef5d96ed69235cd126d03814d24cc00a
Sakura <Sakura@163.com>
2020-01-31 18:15:54 +0800

    feat：登录界面完成

---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    Home
+    <el-button type="info" @click="logout">退出</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout () {
+      window.sessionStorage.clear()
+      this.$message.success('退出成功！')
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+
+</style>
